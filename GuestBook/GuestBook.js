51f0fbe908f072823a65172273f0d9749e00c89e
andriy-g <drey.galygin@gmail.com>
2016-02-17 18:02:26 -0800

    Name field is fixed

--- GuestBook/GuestBook.js
+++ GuestBook/GuestBook.js
@@ -10,8 +10,7 @@ Router.route('/messages/:_id', function() {
 },
 {
   name: 'message.show'
-}
-);
+});
 
 Router.route('/', function() {
   this.render('GuestBook');
@@ -22,6 +21,8 @@ Router.route('/about', function () {
     this.render('about'); //render about template
     this.layout('layout');    //set the main layout template
 });
+
+
 
 
 if (Meteor.isClient) {
@@ -41,8 +42,9 @@ if (Meteor.isClient) {
 
         var messageBox = $(event.target).find('textarea[name=GuestBookMessage]');
         var messageText = messageBox.val();
-        var nameBox = $(event.target).find('input[name=GuestName]');
-        var nameText = nameBox.val();
+        // var nameBox = $(event.target).find('input[name=GuestName]');
+        // var nameText = nameBox.val();
+        var nameText = Meteor.user().username;
 
         if(nameText.length > 0 &&
             messageText.length > 0) {
@@ -64,6 +66,11 @@ if (Meteor.isClient) {
       }
     }
   );
+  Accounts.ui.config({
+    //options are listed in book on p. 135
+    //for email only change to EMAIL_ONLY, USERNAME_AND_EMAIL
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 if (Meteor.isServer) {
@@ -71,5 +78,23 @@ if (Meteor.isServer) {
     Meteor.publish("messages", function() {
         return Messages.find();
       });
+  });
+
+  Accounts.onCreateUser(function(options, user) {
+
+    //when user gets created, it may have some options
+    //that come along with it. For example, Facebook
+    //accounts have extra properties that get stroes
+    //in the options object
+
+    //The user object is where we want to store profile
+    //information for us to access in our meteor app.
+      if(options.profile) {
+        user.profile = options.profile;
+      }
+      else {
+        user.profile = {};
+      }
+      return user;
   });
 }
